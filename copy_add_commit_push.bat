f9a518b2345714429e7cbb784ba1194ef90fa2b4
thanhlongzzz <longvipzzz@gmail.com>
2021-08-17 19:01:23 +0700

    New release

--- copy_add_commit_push.bat
+++ copy_add_commit_push.bat
@@ -6,6 +6,7 @@ rem /e option is needed to copy also all folders and subfolders
 xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\app\release\release\app-release.apk" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\app-release.apk*" /i /e
 xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\app\release\release\output-metadata.json" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\output-metadata.json*" /i /e
 xcopy /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\new_release_note.txt" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\new_release_note.txt" /i /e
+xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchProUpdater\app\release\app-release.apk" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\ATTP_updater.apk*" /i /e
 
 git add -A
 git status
