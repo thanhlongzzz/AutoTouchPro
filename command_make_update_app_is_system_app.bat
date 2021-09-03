87705f5c0afd7d930cb5e6de69ff93d3647e9ea9
thanhlongzzz <longvipzzz@gmail.com>
2021-09-03 22:43:25 +0700

    New release

--- command_make_update_app_is_system_app.bat
+++ command_make_update_app_is_system_app.bat
@@ -1,6 +1,12 @@
+adb push privapp-permissions-attp-updater.xml /sdcard/privapp-permissions-attp-updater.xml
 adb push ATTP_updater.apk /sdcard/ATTP_updater.apk
 adb shell "su -c 'mount -o rw,remount /'"
 adb shell "su -c 'mount -o rw,remount /system'"
+adb shell "su -c 'mount -o rw,remount /etc'"
+adb shell "su -c 'chmod 755 /etc'"
+adb shell "su -c 'chmod 755 /etc/permissions'"
+adb shell "su -c 'mv /sdcard/privapp-permissions-attp-updater.xml /etc/permissions'"
+adb shell "su -c 'chmod 644 /etc/permissions/privapp-permissions-attp-updater.xml'"
 adb shell "su -c 'chmod 755 /system/priv-app/'"
 adb shell "su -c 'mv /sdcard/ATTP_updater.apk /system/priv-app'"
 adb shell "su -c 'chmod 644 /system/priv-app/ATTP_updater.apk'"
