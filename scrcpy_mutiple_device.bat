ba19fa2678c118ca0a159f0b2ee4f80723641bb6
thanhlongzzz <longvipzzz@gmail.com>
2021-10-18 00:22:06 +0700

    New release

---
+++ scrcpy_mutiple_device.bat
@@ -0,0 +1,13 @@
+setlocal ENABLEDELAYEDEXPANSION
+set d=
+FOR /F "tokens=* USEBACKQ" %%F IN (`adb devices`) DO (
+	::lấy danh sách device dạng: "abc  device"
+	set "d=%%F"
+	::set d=!d!,%%F 
+	:: replace chữ device thành -S để tắt màn hình device
+	call set "result=%%d:device=%%"
+	start cmd /C "scrcpy -s !result! -S"
+)
+	::echo %result%
+
+pause
