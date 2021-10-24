b8ac9a30cf8cbc8f424168198716e23981e45d2e
thanhlongzzz <longvipzzz@gmail.com>
2021-10-24 23:29:16 +0700

    New release

---
+++ run_direct_script.bat
@@ -0,0 +1,4 @@
+::soạn thảo file script với đuôi atp xong chuột phải openwwith run_direct_script.bat -> chọn alway , sau chạy script vừa soạn xong chỉ cần click dup vào file atp
+
+adb push %~n1%~x1 /sdcard/AutoTouchPro
+adb shell am start-service -n com.tzappstudio.autotouchpro/.services.task.RunService --es file "%~n1%~x1"
