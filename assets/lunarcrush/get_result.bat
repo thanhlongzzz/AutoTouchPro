507866938d0001a3abe59e8d8acb9577c1507052
thanhlongzzz <longvipzzz@gmail.com>
2021-10-26 00:01:42 +0700

    New release

---
+++ assets/lunarcrush/get_result.bat
@@ -0,0 +1,1 @@
+adb pull /sdcard/AutoTouchPro/data.csv
--- run_direct_script.bat
+++ run_direct_script.bat
@@ -1,4 +1,5 @@
 ::soạn thảo file script với đuôi atp xong chuột phải openwwith run_direct_script.bat -> chọn alway , sau chạy script vừa soạn xong chỉ cần click dup vào file atp
 
 adb push %~n1%~x1 /sdcard/AutoTouchPro
+adb shell am start-service -n com.tzappstudio.autotouchpro/.services.task.RunService --es command "stop"
 adb shell am start-service -n com.tzappstudio.autotouchpro/.services.task.RunService --es file "%~n1%~x1"
