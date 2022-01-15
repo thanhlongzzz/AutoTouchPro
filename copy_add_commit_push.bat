25503315f80e253426fbf89c3a6a61eb6fa42e50
Long Nguyễn <long100197@gmail.com>
2022-01-15 18:06:14 +0700

    New release

--- copy_add_commit_push.bat
+++ copy_add_commit_push.bat
@@ -3,11 +3,11 @@ rem you could also remove the line above, because it might help you to see what 
 
 rem /i option is needed to avoid the batch file asking you whether destination folder is a file or a folder
 rem /e option is needed to copy also all folders and subfolders
-xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\app\README.md" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\README.md*" /i
-xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\app\release\release\app-release.apk" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\app-release.apk*" /i
-xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\app\release\release\output-metadata.json" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\output-metadata.json*" /i
-xcopy /Q /Y /F "D:\WORK\Android\Project\AutoTouchPro\new_release_note.txt" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\new_release_note.txt" /i
-xcopy /S /Q /Y /F "D:\WORK\Android\Project\AutoTouchProUpdater\app\release\app-release.apk" "D:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\ATTP_updater.apk*" /i /e
+xcopy /S /Q /Y /F "E:\WORK\Android\Project\AutoTouchPro\app\README.md" "E:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\README.md*" /i
+xcopy /S /Q /Y /F "E:\WORK\Android\Project\AutoTouchPro\app\release\release\app-release.apk" "E:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\app-release.apk*" /i
+xcopy /S /Q /Y /F "E:\WORK\Android\Project\AutoTouchPro\app\release\release\output-metadata.json" "E:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\output-metadata.json*" /i
+xcopy /Q /Y /F "E:\WORK\Android\Project\AutoTouchPro\new_release_note.txt" "E:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\new_release_note.txt" /i
+xcopy /S /Q /Y /F "E:\WORK\Android\Project\AutoTouchProUpdater\app\release\app-release.apk" "E:\WORK\Android\Project\AutoTouchProUpdateRelease\AutoTouchPro\ATTP_updater.apk*" /i /e
 
 git add -A
 ::git reset -- assets/*
